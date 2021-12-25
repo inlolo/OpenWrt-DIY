648290ec4b570fecd56f9e18e9984f006b2be24e
inlolo <inlolo@gmail.com>
2021-12-25 15:34:15 +0800

    Create diy-part4.sh

---
+++ scripts/diy-part4.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# 修复核心及添加温度显示
+sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+
+# Add luci-app-ssr-plus
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+popd
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
+# Add Lienol's Packages
+git clone --depth=1 https://github.com/Lienol/openwrt-package
+rm -rf ../lean/luci-app-kodexplorer
+
+# Add luci-app-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+
+# Add luci-app-vssr <M> 主流协议和多种自定义视频分流服务-目前只适配最新版 argon主题
+#git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
+#git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
+
+# Add luci-proto-minieap
+#git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
+
+# Add ServerChan 用于 OpenWRT/LEDE 路由器上进行 微信/Telegram 推送的插件
+#git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+
+# Add OpenClash
+#git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+
+# Add luci-app-onliner 通过arp实现的在线人员查看的luci界面
+#git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
+
+# Add luci-app-diskman 一个简单的磁盘管理 LuCI 插件，支持磁盘分区、格式化，支持 RAID / btrfs-raid / btrfs-快照管理
+#git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
+#mkdir parted
+#cp luci-app-diskman/Parted.Makefile parted/Makefile
+
+# Add luci-app-dockerman
+#rm -rf ../lean/luci-app-docker
+#git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
+#git clone --depth=1 https://github.com/lisaac/luci-lib-docker
+
+# Add luci-theme-argon openwrt界面
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+rm -rf ../lean/luci-theme-argon
+
+# Add subconverter 在各种订阅格式之间转换的实用程序
+#git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
+
+# Add luci-udptools UDP测速
+#svn co https://github.com/zcy85611/Openwrt-Package/trunk/luci-udptools
+#svn co https://github.com/zcy85611/Openwrt-Package/trunk/udp2raw
+#svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder-tunnel
+
+# Add OpenAppFilter 应用过滤是一款基于OpenWrt的家长管理插件，支持游戏、视频、聊天、下载等app过滤
+#git clone --depth 1 -b oaf-3.0.1 https://github.com/destan19/OpenAppFilter.git
+
+# Add luci-app-oled (R2S Only)特殊信息显示-温度 cpu频率 网速等
+git clone --depth=1 https://github.com/NateLol/luci-app-oled
+
+# Add extra wireless drivers
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8821cu
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8188eu
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8192du
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl88x2bu
+
+# Add apk (Apk Packages Manager)
+svn co https://github.com/openwrt/packages/trunk/utils/apk
+popd
+
+# Mod zzz-default-settings
+pushd package/lean/default-settings/files
+sed -i '/http/d' zzz-default-settings
+export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
+popd
+
+# Use Lienol's https-dns-proxy package
+pushd feeds/packages/net
+rm -rf https-dns-proxy
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy
+popd
+
+# Use snapshots' syncthing package
+pushd feeds/packages/utils
+rm -rf syncthing
+svn co https://github.com/openwrt/packages/trunk/utils/syncthing
+popd
+
+# Fix mt76 wireless driver
+pushd package/kernel/mt76
+sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+popd
+
+# Change default shell to zsh
+sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
